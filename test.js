c09c3de6a6afffaa68e5c321e8529c6602686de1
laudebugs <lbugasu@gmail.com>
2022-09-11 21:51:47 -0400

    fix: add inputs

--- test.js
+++ test.js
@@ -1,17 +1,22 @@
 require('dotenv').config()
+const { Octokit } = require('@octokit/core')
 
 const octokit = new Octokit({
     auth: process.env.PAT,
 })
 
-export async function sendDownloadRequest(url, tags) {
-    return octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+async function sendDownloadRequest(url, tags) {
+    return octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
         owner: 'laudebugs',
         repo: 'les-articles',
-        event_type: 'download-webpage',
-        client_payload: {
+        workflow_id: 'download-webpage',
+        inputs: {
             url,
             tags,
         },
     })
 }
+
+sendDownloadRequest('https://www.joshwcomeau.com/react/why-react-re-renders/', ['react', 'web-dev'])
+
+module.exports = { sendDownloadRequest }
