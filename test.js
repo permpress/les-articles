9c8cd44dce8f1d7a19c3e234f51092de81448ef1
laudebugs <lbugasu@gmail.com>
2022-09-11 23:20:42 -0400

    add concurrency and pull

--- test.js
+++ test.js
@@ -17,7 +17,62 @@ async function sendDownloadRequest(url, tags) {
         },
     })
 }
+let jobs = [
+    {
+        url: 'https://simonwillison.net/2022/Aug/29/stable-diffusion/',
+        tags: ['ai', 'machine-learning', 'software-engineering'],
+    },
+    {
+        url: 'https://www.browserlondon.com/blog/2022/08/30/why-user-stories-belong-in-the-garbage/',
+        tags: ['dev-ops', 'software-engineering'],
+    },
+    {
+        url: 'https://wagslane.dev/posts/no-one-does-devops/',
+        tags: ['dev-ops', 'software-engineering'],
+    },
+    {
+        url: 'https://jvns.ca/blog/2022/08/30/a-way-to-categorize-debugging-skills/',
+        tags: ['software-engineering', 'debugging'],
+    },
+    {
+        url: 'https://martinheinz.dev/blog/79',
+        tags: ['python', 'software-engineering'],
+    },
+    {
+        url: 'https://blog.southparkcommons.com/move-fast-or-die/',
+        tags: ['entrepreneurship', 'software-engineering'],
+    },
+    {
+        url: 'https://collabfund.com/blog/save-like-a-pessimist-invest-like-an-optimist/',
+        tags: ['investmant', 'life-hacks'],
+    },
+    {
+        url: 'http://paulgraham.com/fr.html?utm_source=pocket_mylist',
+        tags: ['start-ups', 'investment'],
+    },
+    {
+        url: 'https://www.piratewires.com/p/jump-23d06adb4cb7',
+        tags: ['life-hacks'],
+    },
+    {
+        url: 'https://matthewbilyeu.com/blog/2022-09-01/responding-to-recruiter-emails-with-gpt-3',
+        tags: ['ai', 'communication', 'software-engineering'],
+    },
+    {
+        url: 'https://blog.ploeh.dk/2020/09/14/we-need-young-programmers-we-need-old-programmers/',
+        tags: ['software-engineering', 'programming'],
+    },
+    {
+        url: 'https://alexanderwales.com/the-ai-art-apocalypse/',
+        tags: ['ai', 'machine-learning', 'software-engineering'],
+    },
+    {
+        url: 'https://lucasfcosta.com/2022/08/07/how-to-improve-daily-standups.html',
+        tags: ['dev-ops'],
+    },
+]
 
-sendDownloadRequest('https://www.joshwcomeau.com/react/why-react-re-renders/', ['react', 'web-dev'])
+jobs.map(({ url, tags }) => sendDownloadRequest(url, tags))
+// sendDownloadRequest('https://cooking.nytimes.com/recipes/1018097-kimchi-fried-rice', ['cooking', 'recipes'])
 
 module.exports = { sendDownloadRequest }
