ea151cab57272528bae9ab6dec071f75c76042ac
laudebugs <lbugasu@gmail.com>
2022-09-11 22:40:04 -0400

    add condition

--- src/get-path.ts
+++ src/get-path.ts
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk'
 import { getPath } from './helpers'
 
 const url = process.argv[2]
 
-if (!url) {
-    console.error(`${chalk.red('✗')} Please provide a URL.`)
-    process.exit(1)
+if (url) {
+    console.log(getPath(url))
 }
-
-console.log(getPath(url))
